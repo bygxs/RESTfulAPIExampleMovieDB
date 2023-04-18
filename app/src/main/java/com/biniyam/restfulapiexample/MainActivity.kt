74b5a1557b1707dc9852a3985b5329fa0e02dc69
bygxs <114972065+bygxs@users.noreply.github.com>
2023-04-18 14:42:16 +0200

    Last commit Everything works

--- app/src/main/java/com/biniyam/restfulapiexample/MainActivity.kt
+++ app/src/main/java/com/biniyam/restfulapiexample/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         CoroutineScope(Dispatchers.IO).launch {
-           imageBaseUrl = getImageBasUrlFromConfig()
-           println("imageBaseUrl = ${imageBaseUrl}")
+            imageBaseUrl = getImageBasUrlFromConfig()
+            println("imageBaseUrl = ${imageBaseUrl}")
         }
 
 
@@ -37,31 +37,31 @@ class MainActivity : AppCompatActivity() {
 
             CoroutineScope(Dispatchers.Main).launch {
                 //val movie = getMovieDetails(movieId)
-               // println("movie = ${movie}")
+                // println("movie = ${movie}")
 
-           val movie = getMovieFromSearch(query)
+                val movie = getMovieFromSearch(query)
 
-                 loadImage(movie.imagePath)
-                 binding.tvMovieDetails.text = movie.overview
+                loadImage(movie.imagePath)
+                binding.tvMovieDetails.text = movie.overview
                 binding.tvTitle.text = movie.title
 
             }
         }
     }
 
-       suspend fun getMovieDetails(movieId: Int): MovieModel {
+    suspend fun getMovieDetails(movieId: Int): MovieModel {
 
-            val retrofit = Retrofit.Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl(getString(R.string.BASE_URL))
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-            val movieAPIService = retrofit.create(MovieAPIService::class.java)
-            return  movieAPIService.getMovieDetails(movieId,getString(R.string.api_key))
+        val movieAPIService = retrofit.create(MovieAPIService::class.java)
+        return movieAPIService.getMovieDetails(movieId, getString(R.string.api_key))
 
-        }
+    }
 
-   suspend fun getImageBasUrlFromConfig(): String {
+    suspend fun getImageBasUrlFromConfig(): String {
         val retrofit = Retrofit.Builder()
             .baseUrl(getString(R.string.BASE_URL))
             .addConverterFactory(GsonConverterFactory.create())
@@ -69,18 +69,18 @@ class MainActivity : AppCompatActivity() {
 
         val movieAPIService = retrofit.create(MovieAPIService::class.java)
 
-         val response = movieAPIService.getConfigAaJson(getString(R.string.api_key)).body()
+        val response = movieAPIService.getConfigAaJson(getString(R.string.api_key)).body()
 
-       val imageBaseUrl = response?.getAsJsonObject("images")?.get("secure_base_url")?.asString
+        val imageBaseUrl = response?.getAsJsonObject("images")?.get("secure_base_url")?.asString
 
-       val imageSize = response
-           ?.getAsJsonObject("images")
-           ?.getAsJsonArray("poster_sizes")
-           ?.get(4)?.asString
-       println(imageBaseUrl+imageSize)
-       return imageBaseUrl + imageSize
+        val imageSize = response
+            ?.getAsJsonObject("images")
+            ?.getAsJsonArray("poster_sizes")
+            ?.get(4)?.asString
+        println(imageBaseUrl + imageSize)
+        return imageBaseUrl + imageSize
 
-      }
+    }
 
     fun loadImage(imageUrl: String) {
         var image: Bitmap? = null
@@ -102,25 +102,26 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-   suspend fun getMovieFromSearch(query: String): MovieModel{
-       val retrofit = Retrofit.Builder()
-           .baseUrl(getString(R.string.BASE_URL))
-           .addConverterFactory(GsonConverterFactory.create())
-           .build()
+    suspend fun getMovieFromSearch(query: String): MovieModel {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(getString(R.string.BASE_URL))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-       val movieAPIService = retrofit.create(MovieAPIService::class.java)
+        val movieAPIService = retrofit.create(MovieAPIService::class.java)
 
-       val response = movieAPIService.searchByTitle(getString(R.string.api_key),query).body() //.code(),.header()
-       println("movieJson = ${response}")
-       val movieJson = response?.getAsJsonArray("results")?.get(0)
-       println("movieJson = ${movieJson}")
+        val response = movieAPIService.searchByTitle(getString(R.string.api_key), query)
+            .body() //.code(),.header()
+        println("movieJson = ${response}")
+        val movieJson = response?.getAsJsonArray("results")?.get(0)
+        println("movieJson = ${movieJson}")
 
-       val gson = Gson()
-       val movie = gson.fromJson(movieJson, MovieModel::class.java)
-       println("movie objekt = ${movie}")
+        val gson = Gson()
+        val movie = gson.fromJson(movieJson, MovieModel::class.java)
+        println("movie objekt = ${movie}")
 
-       return movie
-
-   }
+        return movie
 
     }
+
+}
